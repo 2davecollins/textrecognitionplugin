687c4a2200805892aca25d6dcdf424957bd9dc34
David Collins <dcollins2@staff.ncirl.ie>
2019-06-27 11:11:17 +0100

    init version 1.0.0

---
+++ www/plugin.js
@@ -0,0 +1,14 @@
+const exec = require('cordova/exec');
+const PLUGIN_NAME = 'MlKitPlugin';
+
+module.exports = {
+    getText: function (img, options, success, error) {
+      exec(success, error, PLUGIN_NAME, 'getText', [img]);
+    },
+    getTextCloud: function (img, options, success, error) {
+      options = options || {};
+      var language = options.language || '';
+  
+      exec(success, error, PLUGIN_NAME, 'getTextCloud', [img, language]);
+    }
+};
